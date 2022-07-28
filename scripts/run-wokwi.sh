0969fcd15062159b1df7def610069caa77c09a05
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-07-28 09:03:39 +0000

    Update wokwi arguments

--- scripts/run-wokwi.sh
+++ scripts/run-wokwi.sh
@@ -28,9 +28,9 @@ fi
 export ESP_ARCH=xtensa-esp32-espidf
 
 # TODO: Update with your Wokwi Project
-export WOKWI_PROJECT_ID=""
+export WOKWI_PROJECT_ID="331029349485511251"
 if [ "${WOKWI_PROJECT_ID}" == "" ]; then
     wokwi-server --chip esp32 target/${ESP_ARCH}/${BUILD_MODE}/mch
 else
-    wokwi-server --chip esp32 --id ${WOKWI_PROJECT_ID} target/${ESP_ARCH}/${BUILD_MODE}/mch
+    wokwi-server --chip esp32 --id ${WOKWI_PROJECT_ID} target/${ESP_ARCH}/${BUILD_MODE}/mch2022-badge-modplayer
 fi
